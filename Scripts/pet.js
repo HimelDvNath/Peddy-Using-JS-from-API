8b6dcfefcd14ecf185253e209c5a8a0e3dc8d0dc
HimelDvNath <himelcse005@gmail.com>
2025-09-12 16:42:36 +0600

    added all pet cards

--- Scripts/pet.js
+++ Scripts/pet.js
@@ -1,27 +1,101 @@
-// petCategories 
-const petCategories = () =>{
-    fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    .then(res => res.json())
-    .then(data => loadCategories(data.categories))
-    .catch(err => console.log(err))
+// petCategories
+const petCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then((res) => res.json())
+    .then((data) => loadCategories(data.categories))
+    .catch((err) => console.log(err));
 };
+// fetch all pets
+const loadAllPets = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/pets")
+    .then((res) => res.json())
+    .then((data) => displayAllPets(data.pets))
+    .catch((err) => console.log(err));
+};
+
 // {
 //     "id": 1,
 //     "category": "Cat",
 //     "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
 // }
-const loadCategories = (pets) =>{
-    const adoptPetsBtnContainer = document.getElementById('categories-btn');
-    for(const pet of pets){
-        const btn = document.createElement('div');
-        btn.classList = 'btn text-gray-200 rounded-md p-10'
-        btn.innerHTML =  `
+const loadCategories = (pets) => {
+  const adoptPetsBtnContainer = document.getElementById("categories-btn");
+  for (const pet of pets) {
+    const btn = document.createElement("div");
+
+    btn.classList = "btn text-gray-200 rounded-md p-7 md:p-10";
+    btn.innerHTML = `
             <button class=" flex gap-5 items-center">
-                <img src=${pet.category_icon}/>
-                <h2 class="text-3xl text-black font-bold">${pet.category}</h2>
+                <img class="h-10 md:h-auto" src=${pet.category_icon}/>
+                <h2 class="text-lg md:text-3xl text-black font-bold">${pet.category}</h2>
             </button>
         `;
-        adoptPetsBtnContainer.append(btn);
-    }
-}
+    adoptPetsBtnContainer.append(btn);
+  }
+};
+// {
+//     "petId": 1,
+//     "breed": "Golden Retriever",
+//     "category": "Dog",
+//     "date_of_birth": "2023-01-15",
+//     "price": 1200,
+//     "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
+//     "gender": "Male",
+//     "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
+//     "vaccinated_status": "Fully",
+//     "pet_name": "Sunny"
+// }
+
+const displayAllPets = (pets) => {
+  const petsCardContainer = document.getElementById("pets-cards");
+
+  for (const pet of pets) {
+    const birthYear = new Date(pet.date_of_birth).getFullYear();
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <div class="card bg-base-100 w-80 shadow-lg p-5">
+            <figure class="rounded-xl mb-2">
+                <img
+                src= ${pet.image}
+                alt="Shoes" />
+            </figure>
+            <div class="">
+                
+                <h2 class="card-title text-xl mb-2">
+                ${pet.pet_name}
+                </h2>
+
+                <div class="flex gap-2 mb-1">
+                <img class="h-5" src="https://img.icons8.com/?size=48&id=Roa2QOIOdmUA&format=png" />
+                <p class="text-md text-gray-500">Breed: ${pet.breed}</p>
+                </div>
+                
+                <div class="flex gap-2 mb-1">
+                <img class="h-5" src="https://img.icons8.com/?size=160&id=udduMUcrHmZa&format=png" />
+                <p class="text-md text-gray-500">Birth: ${birthYear}</p>
+                </div>
+                 
+                <div class="flex gap-2 mb-1">
+                <img class="h-5" src="https://img.icons8.com/?size=48&id=skdvszUmNlnQ&format=png" />
+                <p class="text-md text-gray-500">Gender: ${pet.gender}</p>
+                </div>
+                 
+                <div class="flex gap-2 mb-1">
+                <img class="h-5" src="https://img.icons8.com/?size=48&id=89392&format=png" />
+                <p class="text-md text-gray-500">Price: ${pet.price}</p>
+                </div>
+                <div class="divider"></div>
+                <div class="flex justify-between">
+                <div class="btn p-5"><img class="h-8" src="https://img.icons8.com/?size=96&id=U6uSXVbuA1xU&format=png"/></div>
+                <div class="btn p-5 text-blue-600 font-bold">Adopt</div>
+                <div class="btn p-5 text-blue-600 font-bold">Details</div>
+                </div>
+            </div>
+        </div>
+    `;
+    petsCardContainer.append(card);
+  }
+};
+
 petCategories();
+loadAllPets();
