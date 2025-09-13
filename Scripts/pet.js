2de996c4f37b0d4ec2b317c4d697f0cd65a46640
HimelDvNath <himelcse005@gmail.com>
2025-09-14 02:41:18 +0600

    fixed some issue

--- Scripts/pet.js
+++ Scripts/pet.js
@@ -47,10 +47,10 @@ const notAvailablePets = (petsCardContainer) => {
   petsCardContainer.classList.remove("grid");
   const hero = document.createElement("div");
   hero.innerHTML = `
-  <div class="hero bg-base-200 flex justify-center items-center mx-auto p-40">
-  <div class="hero-content text-center">
-  <div class="">
-  <div class="flex justify-center items-center"><img class="h-40 w-40 mb-5 " src="images/error.webp"/></div>
+        <div class="hero bg-base-200 flex justify-center items-center mx-auto p-40">
+        <div class="hero-content text-center">
+        <div class="">
+        <div class="flex justify-center items-center"><img class="h-40 w-40 mb-5 " src="images/error.webp"/></div>
           <h1 class="text-5xl font-extrabold">No Information Available</h1>
           <p class="py-6 min-w-md">
             It is a long established fact that a reader will be distracted by the readable content of a page when looking at <br>its layout. The point of using Lorem Ipsum is that it has a.
@@ -124,18 +124,7 @@ const detailsBtnClicked = (petID)=>{
   .then(data => displayModal(data.petData))
   .catch(err => console.log(err))
 }
-// {
-//     "petId": 1,
-//     "breed": "Golden Retriever",
-//     "category": "Dog",
-//     "date_of_birth": "2023-01-15",
-//     "price": 1200,
-//     "image": "https://i.ibb.co.com/p0w744T/pet-1.jpg",
-//     "gender": "Male",
-//     "pet_details": "This friendly male Golden Retriever is energetic and loyal, making him a perfect companion for families. Born on January 15, 2023, he enjoys playing outdoors and is especially great with children. Fully vaccinated, he's ready to join your family and bring endless joy. Priced at $1200, he offers love, loyalty, and a lively spirit for those seeking a playful yet gentle dog.",
-//     "vaccinated_status": "Fully",
-//     "pet_name": "Sunny"
-// }
+
 const displayModal = (pet)=>{
   const modal = document.getElementById('custom-modal');
   const birthYear = new Date(pet.date_of_birth).getFullYear();
@@ -239,5 +228,8 @@ document.getElementById("clean-btn").addEventListener("click", () => {
   chosenPetsContainer.innerHTML = "";
 });
 
+document.getElementById('view-more-btn').addEventListener('click', ()=>{
+  document.getElementById("all-pets").scrollIntoView({ behavior: "smooth" });
+})
 petCategories();
 loadAllPets();
